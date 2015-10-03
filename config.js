e75db3ccd644b00a7bf95be86bf890a89937c8e0
dss <digital.synapse.software@gmail.com>
2015-10-03 15:22:29 -0700

    inital commit

---
+++ config.js
@@ -0,0 +1,53 @@
+System.config({
+  baseURL: ".",
+  defaultJSExtensions: true,
+  transpiler: "typescript",
+  typescriptOptions: {
+    "noImplicitAny": true,
+    "typeCheck": true,
+    "resolveAmbientRefs": true
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  packages: {
+    "app": {
+      "main": "main",
+      "defaultExtension": "ts",
+      "meta": {
+        "*.ts": {
+          "loader": "ts"
+        }
+      }
+    }
+  },
+
+  map: {
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.25",
+    "core-js": "npm:core-js@1.2.1",
+    "opsuite/webef": "github:opsuite/webef@master",
+    "ts": "github:frankwallis/plugin-typescript@2.0.17",
+    "typescript": "npm:typescript@1.6.2",
+    "webef": "github:opsuite/webef@master",
+    "github:frankwallis/plugin-typescript@2.0.17": {
+      "typescript": "npm:typescript@1.6.2"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "github:opsuite/webef@master": {
+      "google/lovefield": "github:google/lovefield@master"
+    },
+    "npm:babel-runtime@5.8.25": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.2.1": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    }
+  }
+});
---
+++ readme.txt
@@ -0,0 +1,25 @@
+﻿name:	    ds.min.js		
+version:    0.2a
+date:	    02/28/2015	
+author:     digital-synapse
+
+ A very fast class framework. Allows OOP concepts in javascript including classes, inheritance, multi-inheritance, polymophism, interfaces, and enumerators without using TypeScript. Provides scope isolation via dependency injection in modules, controllers, and factories. Also very small at only 3 kb for the minified version. (compare that to prototypejs minified 93.38 kb)
+
+.: Getting Started :.
+
+To get started grab ds.min.js (This is the minified oop library)
+Then head on over to the wiki @ http://github.com/digital-synapse/ds.oop/wiki to view the guide and documentation.
+
+.: New Features :.
+
+- dependency injection
+- support for modules, factories, and controllers
+- namespaces removed (replaced with modules) 
+- constructorless classes and static classes
+
+
+.: Support :.
+
+If you like this project or have some questions feel free to send me an email:
+
+    digital.synapse.software [ಠ_ಠ] gmail.com
